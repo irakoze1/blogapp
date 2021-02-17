803891f6172d8bc5cc5318e09f6909afe3d71b00
irakoze1 <irakozetititof@gmail.com>
2021-02-17 02:53:23 +0200

    creation blogapp with react graphql and node

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navigation from './components/navigation'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom' 
+
+import PageRenderer from './page-renderer'
+
+function App() {
+  const user = {
+    firstName: 'Tititof',
+    lastName: 'Irakoze'
+  }
+  return (
+    <Router>
+      <div className="App">
+        <Navigation user={user} />
+        <Switch>
+          <Route path="/:page" component={PageRenderer} />
+          <Route path="/" render={() => <Redirect to="/home" />} />
+          <Route component={() => 404} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
